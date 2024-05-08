0391ba5f8a8c00f230b3160957d26abdfbacbb48
Qingyu255 <qingyu2555@gmail.com>
2024-05-09 01:08:49 +0800

    added chart component: Visualise Trends Across Semesters

--- src/components/interactiveCharts/VisualiseTrendAcrossSemesters.tsx
+++ src/components/interactiveCharts/VisualiseTrendAcrossSemesters.tsx
@@ -1,35 +1,12 @@
-"use client"
 import React, { useState, useEffect } from 'react'
 import LineChart from '@/components/charts/LineChart'
-import BarChart from '@/components/charts/BarChart'
 import DropDown from '@/components/DropDown'
-import VisualiseTrendAcrossSemesters from '@/components/interactiveCharts/VisualiseTrendAcrossSemesters'
 
-// const dummyData = {
-//     title: "Loading",
-//     x_axis_label: "Bidding Window",
-//     y_axis_label: "Bid Price",
-//     x_axis_values: ["Loading"], // Array of labels
-//     datasets: [
-//       {
-//         data: [0], // Array of data values
-//         backgroundColor: "rgba(75, 192, 192, 0.6)", // Background color for the bars
-//         borderColor: "rgba(75, 192, 192, 1)", // Border color for the bars
-//         borderWidth: 2, // Border width for the bars
-//       }
-//     ]
-// }
-
-export default function page({ params } : {params: {courseCode: string}}) {
-
+export default function VisualiseTrendAcrossSemesters({courseCode} : {courseCode: string}) {
     const apiURL = "http://127.0.0.1:8000"
 
-    const courseCode: string = params.courseCode.toUpperCase()
-    // const [courseCode, setCourseCode] = useState<string>(params.courseCode)
-    const [chartDataOverview, setChartDataOverview] = useState<any>(null)
-    const [chartDataInstructorOverview, setChartDataInstructorOverview] = useState<any>(null)
     const [error, setError] = useState<any>(null)
-    
+
     const [courseInstructorsDropdownArr, setCourseInstructorsDropdownArr] = useState<string[]>()
     const [courseInstructorSelected, setCourseInstructorSelected] = useState<string>("")
 
@@ -88,43 +65,7 @@ export default function page({ params } : {params: {courseCode: string}}) {
             setError(error)
             console.error(error)
         }
-        
     }
-
-    useEffect(() => {
-        const fetchCourseMinMaxMeanMedianMedianData = async () => {
-            try {
-                // try to fetch from api end point for the Course Min Max Mean Median Median Data
-                const response = await fetch(`${apiURL}/coursedata/overview/${courseCode}`)
-                if (!response.ok) {
-                    throw new Error(`${response.status}\nCourse not Found`)
-                }
-                const chartData = await response.json()
-                setChartDataOverview(chartData)
-            } catch (error: any) {
-                setError(error)
-                console.error(error)
-            }
-        }
-        const fetch_all_instructor_median_median_bid_by_course_code = async () => {
-            try {
-                // try to fetch from api end point for the Course Min Max Mean Median Median Data
-                const response = await fetch(`${apiURL}/coursedata/overview/instructor_median_bid_chart/${courseCode}`)
-                if (!response.ok) {
-                    throw new Error(`${response.status}\nCourse not Found`)
-                }
-                const chartDataInstructorOverview = await response.json()
-                setChartDataInstructorOverview(chartDataInstructorOverview)
-            } catch (error: any) {
-                setError(error)
-                console.error(error)
-            }
-        }
-
-        fetchCourseMinMaxMeanMedianMedianData()
-        fetch_all_instructor_median_median_bid_by_course_code()
-
-    }, [courseCode]);
 
     useEffect(() => {
         // Fetch dropdown options array for select instructor on page refresh
@@ -144,10 +85,8 @@ export default function page({ params } : {params: {courseCode: string}}) {
         }
         fetch_instructors_who_teach_course_code()
     }, [])
-
     return (
-        <div className='flex flex-col min-h-screen'>
-            <p className='p-8 text-2xl font-bold'>Course Code: {courseCode}</p>
+        <>
             {error ? (
                 <div className="flex items-center p-4 m-4 text-sm rounded-xl text-red-800 bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
                     <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -161,20 +100,32 @@ export default function page({ params } : {params: {courseCode: string}}) {
             ) 
             : (
                 <div className='flex flex-col gap-y-5'>
-                    <div className='flex justify-center'>
-                        <BarChart chartData={chartDataOverview} />
+                    <div className='flex flex-row justify-left mx-20 items-center gap-x-5'>
+                        <DropDown 
+                            category='Instructor'
+                            onSelect={handleInstructorSelect}
+                            options={courseInstructorsDropdownArr}
+                        >
+                        </DropDown>
+                    
+                    {(isBiddingWindowDropdownVisible && biddingWindowDropdownArr.length > 0) && (
+                        <DropDown 
+                            category='Bidding Window'
+                            onSelect={handleBiddingWindowSelect}
+                            options={biddingWindowDropdownArr}
+                        >
+                        </DropDown>
+                    )}
                     </div>
                     <div className='flex justify-center'>
-                        <BarChart chartData={chartDataInstructorOverview} />
+                        {(!hideDetailedCharts && selectedBiddingWindow && chartDataInstructorsBiddingWindow) && (
+                            <LineChart chartData={chartDataInstructorsBiddingWindow} />
+                        )}
                     </div>
-                    <h1 className='text-3xl font-extrabold mx-20'>Visualise Trends Across Semesters</h1>
-                    <VisualiseTrendAcrossSemesters courseCode={courseCode}></VisualiseTrendAcrossSemesters>
                 </div>
-                
             )}
-            
-            
-            
-        </div>
+        </>
     )
 }
+
+
