788f56f2b09d347deff17db76946442106dc4d42
Qingyu255 <qingyu2555@gmail.com>
2024-05-10 02:24:45 +0800

    added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,21 @@
 import React from "react"
 import Link from "next/link"
+import Image from 'next/image'
 import SearchBar from "./SearchBar"
 
 export default function Navbar() {
     return (
         <>
             <div className="flex bg-blue-300 items-center rounded-b-md">
-                <div className="p-5 px-8">
-                    <Link href='/' className="text-2xl font-bold">BOSS Analytics</Link>
-                </div>
+                    <Link href='/' className="flex items-center text-2xl font-bold px-5 py-2 gap-x-2">
+                        <Image
+                            src="/logo.png"
+                            width={50}
+                            height={50}
+                            alt="logo"
+                        />
+                        BOSS Analytics
+                    </Link>
                 <div className="flex justify-end">
                     
                 </div>
